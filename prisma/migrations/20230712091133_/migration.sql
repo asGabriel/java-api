0f06001d279a10d21764c492f2fca5d075c8ce2c
Gabriel Andrade <andrade.gabriel2295@gmail.com>
2025-08-10 10:36:25 -0300

    start project

--- prisma/migrations/20230712091133_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `PHOTO` on the `statement` table. The data in that column could be lost. The data in that column will be cast from `TinyInt` to `Blob`.
-
-*/
--- AlterTable
-ALTER TABLE `statement` MODIFY `PHOTO` BLOB NOT NULL;
--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StatementModule } from './statement/statement.module';
-
-@Module({
-  imports: [StatementModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
